ff24b0104af41427ad614c506ad549e824bfc3b8
isabelly Nóbrega <112141013+isabellynobrega@users.noreply.github.com>
2023-03-03 17:10:14 -0300

    Add files via upload

---
+++ Ex01/ex01.js
@@ -0,0 +1,46 @@
+/*Obtenha dados da altura e o sexo (M OU F) de 15 pessoas e apresente os seguintes resultados
+- A maior e menor altura do grupo;
+- A média de altura dos homens;
+- O número de mulheres.*/
+
+
+
+var q;
+var altura;
+var sexo;
+var maioralt = 0;
+var menoralt = 0;
+var mulher = 0;
+var homem = 0;
+var mediamulher = 0;
+var contamulher = 0;
+var contahomem = 0;
+
+            for(q = 1 ;q <= 15; q++) { //quantidade de vezes a ser perguntado//
+ 
+                sexo = prompt("Qual o seu sexo, Feminino (F) ou Masculino (M)?");                
+                altura = parseInt(prompt("Qual a sua altura["+q+"]:"));
+
+
+
+                if(q == 0) {//maior e menor altura//
+                    menoralt = altura;
+                } if(altura > maioralt) { 
+                    maioralt = altura;
+                } if(altura < menoralt) { 
+                    menoralt = altura;
+                } if(sexo == 'F') {//contagem de Mulheres//
+                    mulher += altura;
+                    contamulher++;
+                } else if(sexo == 'M') {//contegem de Homens//
+                    homem += altura;
+                    contahomem++;
+                }
+            }
+            mediamulher = mulher/contamulher;//media de mulheres//
+           
+            //valores pedidos//
+            alert("A maior altura é:" + maioralt); 
+            alert("A menor altura é:" + menoralt);
+            alert("A média das mulheres é:" + mediamulher);
+            alert("O número de homens é:" + contahomem);
